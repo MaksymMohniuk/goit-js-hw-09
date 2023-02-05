2e0f2555bac76fe92f8c6ce627db8ad175c337e2
MaksymMohniuk <112825661+MaksymMohniuk@users.noreply.github.com>
2023-02-06 00:22:31 +0200

    task-1-correction

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -12,7 +12,7 @@ function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
   };
 
-  function changeColor(color) {
+  function changeColor() {
     document.body.style.background = getRandomHexColor();
 };
 
