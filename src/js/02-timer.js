c1e5eb94dbce545c807d3dbeefc813f8b98769eb
MaksymMohniuk <112825661+MaksymMohniuk@users.noreply.github.com>
2023-02-08 00:08:24 +0200

    update-task-2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -5,7 +5,8 @@ import "flatpickr/dist/flatpickr.min.css";
 flatpickr("input#datetime-picker", {});
 
 const button = document.querySelector('[data-start]');
-console.log(button);
+// console.log(button);
+let countDownDate;
 
 const options = {
     enableTime: true,
@@ -13,6 +14,18 @@ const options = {
     defaultDate: new Date(),
     minuteIncrement: 1,
     onClose(selectedDates) {
+      countDownDate = selectedDates[0];
       console.log(selectedDates[0]);
+
+      if(countDownDate < Date.now) {
+        alert('Please choose a date in the future')
+        return;
+      }
+      if(countDownDate > Date.now) {
+        button.disabled = false;
+        const diff = countDownDate - Date.now;
+        return diff;
+      }
     },
   };
+
