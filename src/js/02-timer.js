8841af942406808d9cb495131f0dd614a815f61c
MaksymMohniuk <112825661+MaksymMohniuk@users.noreply.github.com>
2023-02-08 23:18:48 +0200

    update-task-2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -6,7 +6,13 @@ flatpickr("input#datetime-picker", {});
 
 const button = document.querySelector('[data-start]');
 // console.log(button);
+const outputDays = document.querySelector('[data-days]');
+const outputHours = document.querySelector('[data-hours]');
+const outputMinutes = document.querySelector('[data-minutes]');
+const outputSeconds = document.querySelector('[data-seconds]');
 let countDownDate;
+
+const calendar = flatpickr(input, options);
 
 const options = {
     enableTime: true,
@@ -29,3 +35,42 @@ const options = {
     },
   };
 
+  button.addEventListener('click', startTimer);
+
+  const intervalId = setInterval(startTimer, 1000);
+
+  function startTimer() {
+    const diff = countDownDate - Date.now;
+    const { days, hours, minutes, seconds } = convertMs(diff);
+    outputDays.textContent = `${days}`;
+    outputHours.textContent = `${addZero(hours)}`;
+    outputMinutes.textContent = `${addZero(minutes)}`;
+    outputSeconds.textContent = `${addZero(seconds)}`;
+  }
+
+  function convertMs(ms) {
+    // Number of milliseconds per unit of time
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+  
+    // Remaining days
+    const days = Math.floor(ms / day);
+    // Remaining hours
+    const hours = Math.floor((ms % day) / hour);
+    // Remaining minutes
+    const minutes = Math.floor(((ms % day) % hour) / minute);
+    // Remaining seconds
+    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  
+    return { days, hours, minutes, seconds };
+  }
+
+  function reloadPage() {
+    clearInterval(intervalId);
+  }
+
+  function addZero(Number) {
+    return String(number).padStart(2, 0);
+  }
