3682a904f9c0124acfd423d21eab7b1a9102083d
MaksymMohniuk <112825661+MaksymMohniuk@users.noreply.github.com>
2023-02-11 00:37:25 +0200

    task-3-start

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,5 +1,9 @@
 const firstDelay = document.querySelector('input[name="delay"]');
-console.log(firstDelay);
+const step = document.querySelector('input[name="step"]');
+const amount = document.querySelector('input[name="amount"]');
+const button = document.querySelector('button[type ="submit"]');
+
+button.addEventListener('click', createPromise);
 
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
